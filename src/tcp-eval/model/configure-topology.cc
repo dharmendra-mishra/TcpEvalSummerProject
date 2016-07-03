022c55370c38c3183c2dd43395f2bc43c1301958
Dharmendra Mishra <dharmendra.nitk@gmail.com>
2016-07-03 09:20:18 +0530

    getters and setters added

--- src/tcp-eval/model/configure-topology.cc
+++ src/tcp-eval/model/configure-topology.cc
@@ -1,11 +1,327 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2016 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Dharmendra Kumar Mishra <dharmendra.nitk@gmail.com>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ */
+
+// Implement an object to configure topology in tcp-eval.
 
 #include "configure-topology.h"
+#include "traffic-parameters.h"
+
+#include "ns3/log.h"
+#include "ns3/data-rate.h"
+
+//#include "ns3/core-module.h"
 
 namespace ns3 {
 
-/* ... */
+NS_LOG_COMPONENT_DEFINE ("ConfigureTopology");
 
+NS_OBJECT_ENSURE_REGISTERED (ConfigureTopology);
+
+TypeId
+ConfigureTopology::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::ConfigureTopology")
+    .SetParent<Object> ()
+    .SetGroupName ("TcpEvaluationSuite")
+/*    .AddAttribute ("BottleneckBandwidth",
+                   "Bottleneck link capacity in Mbps",
+                   DoubleValue (10),
+                   MakeDoubleAccessor (&ConfigureTopology::m_bottleneckBandwidth),
+                   MakeDoubleChecker<double> (0))
+    .AddAttribute ("BottleneckCount", "Number of bottleneck links",
+                   UintegerValue (1),
+                   MakeUintegerAccessor (&ConfigureTopology::m_nBottlenecks),
+                   MakeUintegerChecker<uint32_t> ())
+    .AddAttribute ("RTTP",
+                   "Round trip propagation delay in seconds",
+                   TimeValue (Seconds (0.08)),
+                   MakeTimeAccessor (&ConfigureTopology::m_rttp),
+                   MakeTimeChecker ())
+    .AddAttribute ("RttDiff",
+                   "Flow RTT difference in seconds",
+                   TimeValue (Seconds (0)),
+                   MakeTimeAccessor (&ConfigureTopology::m_rttDifference),
+                   MakeTimeChecker ())*/
+  ;
+  return tid;
+}
+
+ConfigureTopology::ConfigureTopology (void)
+{
+  m_tmixEnabled = false;
+  m_centralLinkBufferBdp = 1.0;
+}
+
+ConfigureTopology::~ConfigureTopology (void)
+{
+}
+
+void
+ConfigureTopology::SetTopologyParameters ()
+{
+  m_centralLinkDelay = MilliSeconds ((m_rttp.GetMilliSeconds () * 0.5 * 0.8) / m_nCentralLinks);         //ms
+  m_delayDifference = MilliSeconds ((m_rttDifference.GetMilliSeconds () * 1.0) / 4.0);          //ms
+  m_nonCentralLinkDelay = MilliSeconds ((m_rttp.GetMilliSeconds () * 0.5 * 0.2) / 2.0);          //ms
+  m_nonCentralLinkBandwidth = DataRate (m_centralLinkBandwidth.GetBitRate () * 1.0 * 2);          //Mbps
+
+  if (m_tmixEnabled == true)
+    {
+      m_centralLinkBuffer = (m_centralLinkBufferBdp * (m_centralLinkBandwidth.GetBitRate () / 1000000) * m_bufferLength.GetMilliSeconds ()) / (8.0 * 1.5);
+      m_nonCentralLinkBuffer = m_centralLinkBuffer;
+    }
+  else
+    {
+      // calculations pending due to avg_rtt_
+      //m_centralLinkBuffer =
+      m_nonCentralLinkBuffer = m_centralLinkBuffer;
+    }
+}
+
+void
+ConfigureTopology::SetCentralLinkBandwidth (DataRate centralLinkBandwidth)
+{
+  m_centralLinkBandwidth = centralLinkBandwidth;
+}
+
+DataRate
+ConfigureTopology::GetCentralLinkBandwidth (void) const
+{
+  return m_centralLinkBandwidth;
+}
+
+void
+ConfigureTopology::SetCentralLinkCount (uint32_t nCentralLink)
+{
+  m_nCentralLinks = nCentralLink;
+}
+
+uint32_t
+ConfigureTopology::GetCentralLinkCount (void) const
+{
+  return m_nCentralLinks;
+}
+
+void
+ConfigureTopology::SetRttp (Time rttp)
+{
+  m_rttp = rttp;
+}
+
+Time
+ConfigureTopology::GetRttp (void) const
+{
+  return m_rttp;
+}
+
+void
+ConfigureTopology::SetRttDifference (Time rttDifference)
+{
+  m_rttDifference = rttDifference;
+}
+
+Time
+ConfigureTopology::GetRttDifference (void) const
+{
+  return m_rttDifference;
+}
+
+void
+ConfigureTopology::SetDelayDifference (Time delayDifference)
+{
+  m_delayDifference = delayDifference;
+}
+
+Time
+ConfigureTopology::GetDelayDifference (void) const
+{
+  return m_delayDifference;
+}
+
+void
+ConfigureTopology::SetCentralLinkDelay (Time centralLinkDelay)
+{
+  m_centralLinkDelay = centralLinkDelay;
+}
+
+Time
+ConfigureTopology::GetCentralLinkDelay (void) const
+{
+  return m_centralLinkDelay;
+}
+
+void
+ConfigureTopology::SetNonCentralLinkDelay (Time nonCentralLinkDelay)
+{
+  m_nonCentralLinkDelay = nonCentralLinkDelay;
+}
+
+Time
+ConfigureTopology::GetNonCentralLinkDelay (void) const
+{
+  return m_nonCentralLinkDelay;
+}
+
+void
+ConfigureTopology::SetNonCentralLinkBandwidth (DataRate nonCentralLinkBandwidth)
+{
+  m_nonCentralLinkBandwidth = nonCentralLinkBandwidth;
+}
+
+DataRate
+ConfigureTopology::GetNonCentralLinkBandwidth (void) const
+{
+  return m_nonCentralLinkBandwidth;
+}
+
+void
+ConfigureTopology::SetCentralLinkBuffer (uint32_t centralLinkBuffer)
+{
+  m_centralLinkBuffer = centralLinkBuffer;
+}
+
+uint32_t
+ConfigureTopology::GetCentralLinkBuffer (void) const
+{
+  return m_centralLinkBuffer;
+}
+
+void
+ConfigureTopology::SetNonCentralLinkBuffer (uint32_t nonCentralLinkBuffer)
+{
+  m_nonCentralLinkBuffer = nonCentralLinkBuffer;
+}
+
+uint32_t
+ConfigureTopology::GetNonCentralLinkBuffer (void) const
+{
+  return m_nonCentralLinkBuffer;
+}
+
+void
+ConfigureTopology::SetEdgeDelay (Time edgeDelay)
+{
+  m_edgeDelay = edgeDelay;
+}
+
+Time
+ConfigureTopology::GetEdgeDelay (void) const
+{
+  return m_edgeDelay;
+}
+
+void
+ConfigureTopology::SetEdgeBandwidth (DataRate edgeBandwidth)
+{
+  m_edgeBandwidth = edgeBandwidth;
+}
+
+DataRate
+ConfigureTopology::GetEdgeBandwidth (void) const
+{
+  return m_edgeBandwidth;
+}
+
+void
+ConfigureTopology::SetCoreDelay (Time coreDelay)
+{
+  m_coreDelay = coreDelay;
+}
+
+Time
+ConfigureTopology::GetCoreDelay (void) const
+{
+  return m_coreDelay;
+}
+
+void
+ConfigureTopology::SetTmixEnabled (bool tmixEnabled)
+{
+  m_tmixEnabled = tmixEnabled;
+}
+
+bool
+ConfigureTopology::IsTmixEnabled (void) const
+{
+  return m_tmixEnabled;
+}
+
+void
+ConfigureTopology::SetPrefillT (Time prefillT)
+{
+  m_prefillT = prefillT;
+}
+
+Time
+ConfigureTopology::GetPrefillT (void) const
+{
+  return m_prefillT;
+}
+
+void
+ConfigureTopology::SetPrefillSi (Time prefillSi)
+{
+  m_prefillSi = prefillSi;
+}
+
+Time
+ConfigureTopology::GetPrefillSi (void) const
+{
+  return m_prefillSi;
+}
+
+void
+ConfigureTopology::SetScale (double scale)
+{
+  m_scale = scale;
+}
+
+double
+ConfigureTopology::GetScale (void) const
+{
+  return m_scale;
+}
+
+void
+ConfigureTopology::SetSimulationStopTime (Time simulationStopTime)
+{
+  m_simulationStopTime = simulationStopTime;
+}
+
+Time
+ConfigureTopology::GetSimulationStopTime (void) const
+{
+  return m_simulationStopTime;
+}
+
+void
+ConfigureTopology::SetSimulationEndTime (Time simulationEndTime)
+{
+  m_simulationEndTime = simulationEndTime;
+}
+
+Time
+ConfigureTopology::GetSimulationEndTime (void) const
+{
+  return m_simulationEndTime;
+}
 
 }
 
--- src/tcp-eval/model/configure-topology.h
+++ src/tcp-eval/model/configure-topology.h
@@ -1,11 +1,179 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2016 NITK Surathkal
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: Dharmendra Kumar Mishra <dharmendra.nitk@gmail.com>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ */
+
+// Define an object to configure topology in tcp-eval.
+
 #ifndef CONFIGURE_TOPOLOGY_H
 #define CONFIGURE_TOPOLOGY_H
 
+#include <stdint.h>
+
+#include "ns3/object.h"
+#include "ns3/core-module.h"
+#include "ns3/data-rate.h"
+
 namespace ns3 {
 
-/* ... */
+/**
+ * \brief base class to configure the topology
+ *
+ * It initializes parameters used in the configuration of the topology.
+ * Other classes like Dumbbell and ParkingLot should extend this class.
+ */
+class ConfigureTopology : public Object
+{
+public:
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+  static TypeId GetTypeId (void);
 
+  /**
+   * \brief Constructor
+   */
+  ConfigureTopology (void);
+
+  /**
+   * \brief Destructor
+   */
+  ~ConfigureTopology (void);
+
+  /**
+   * \brief Initializes the topology parameters
+   *
+   * \param traffic Object of TrafficParameters class that contains traffic information
+   * \param nBottlenecks Number of Bottleneck links. For dumbbell it is 1 and for other topologies >= 1
+   */
+  void SetTopologyParameters ();
+
+  void SetCentralLinkBandwidth (DataRate centralLinkBandwidth);
+
+  DataRate GetCentralLinkBandwidth (void) const;
+
+  void SetCentralLinkCount (uint32_t nCentralLink);
+
+  uint32_t GetCentralLinkCount (void) const;
+
+  void SetRttp (Time rttp);
+
+  Time GetRttp (void) const;
+
+  void SetRttDifference (Time rttDifference);
+
+  Time GetRttDifference (void) const;
+
+  void SetDelayDifference (Time delayDifference);
+
+  Time GetDelayDifference (void) const;
+
+  void SetCentralLinkDelay (Time centralLinkDelay);
+
+  Time GetCentralLinkDelay (void) const;
+
+  void SetNonCentralLinkDelay (Time nonCentralLinkDelay);
+
+  Time GetNonCentralLinkDelay (void) const;
+
+  void SetNonCentralLinkBandwidth (DataRate nonCentralLinkBandwidth);
+
+  DataRate GetNonCentralLinkBandwidth (void) const;
+
+  void SetCentralLinkBuffer (uint32_t centralLinkBuffer);
+
+  uint32_t GetCentralLinkBuffer (void) const;
+
+  void SetNonCentralLinkBuffer (uint32_t nonCentralLinkBuffer);
+
+  uint32_t GetNonCentralLinkBuffer (void) const;
+
+  void SetEdgeDelay (Time edgeDelay);
+
+  Time GetEdgeDelay (void) const;
+
+  void SetEdgeBandwidth (DataRate edgeBandwidth);
+
+  DataRate GetEdgeBandwidth (void) const;
+
+  void SetCoreDelay (Time CoreDelay);
+
+  Time GetCoreDelay (void) const;
+
+  void SetTmixEnabled (bool tmixEnabled);
+
+  bool IsTmixEnabled (void) const;
+
+  void SetPrefillT (Time prefillT);
+
+  Time GetPrefillT (void) const;
+
+  void SetPrefillSi (Time prefillSi);
+
+  Time GetPrefillSi (void) const;
+
+  void SetScale (double scale);
+
+  double GetScale (void) const;
+
+  /**
+    * \brief Set the simulation time in seconds
+    *
+    * \param simulationTime Time when the simulation should stop
+    */
+  void SetSimulationStopTime (Time simulationStopTime);
+
+  /**
+    * \brief Get the simulation time in seconds
+    *
+    * \return the simulation time in seconds
+    */
+  Time GetSimulationStopTime (void) const;
+
+  void SetSimulationEndTime (Time simulationEndTime);
+
+  Time GetSimulationEndTime (void) const;
+
+protected:
+  DataRate m_centralLinkBandwidth;      //!< Bandwidth of central link in Mbps
+  uint32_t m_nCentralLinks;             //!< Number of central links.
+  Time     m_rttp;                      //!< Round trip propagation delay in seconds
+  Time     m_rttDifference;             //!< Flow RTT difference in seconds
+  Time     m_delayDifference;           //!< Delay difference in seconds
+  Time     m_centralLinkDelay;           //!< Delay of bottleneck link in seconds
+  DataRate m_nonCentralLinkBandwidth;    //!< Bandwidth of non-bottleneck link in Mbps
+  Time     m_nonCentralLinkDelay;        //!< Delay of non-bottleneck link in seconds
+  double  m_nonCentralLinkBuffer;       //!< Size of the non-bottleneck buffer
+  double   m_centralLinkBufferBdp;       //!< Bandwidth-Delay Product for the bottleneck link
+  double   m_centralLinkBuffer;          //!< Size of the bottleneck buffer
+  Time     m_edgeDelay;
+  DataRate m_edgeBandwidth;
+  Time     m_coreDelay;
+  bool    m_tmixEnabled;
+  Time     m_prefillT;
+  Time     m_prefillSi;
+  double  m_scale;
+  Time     m_simulationStopTime;
+  Time     m_simulationEndTime;
+  Time     m_bufferLength;
+};
 }
 
 #endif /* CONFIGURE_TOPOLOGY_H */
