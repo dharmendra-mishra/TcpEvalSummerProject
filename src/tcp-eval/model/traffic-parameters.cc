75ba05533ff1771019648cbd9af5a41c39a08649
Dharmendra Mishra <dharmendra.nitk@gmail.com>
2016-07-03 09:22:17 +0530

    getters and setters added

--- src/tcp-eval/model/traffic-parameters.cc
+++ src/tcp-eval/model/traffic-parameters.cc
@@ -2,10 +2,129 @@
 
 #include "traffic-parameters.h"
 
+#include "ns3/log.h"
+#include "ns3/core-module.h"
+#include <iostream>
+
 namespace ns3 {
 
-/* ... */
+NS_LOG_COMPONENT_DEFINE ("TrafficParameters");
 
+NS_OBJECT_ENSURE_REGISTERED (TrafficParameters);
+
+TypeId
+TrafficParameters::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::TrafficParameters")
+    .SetParent<Object> ()
+    .SetGroupName ("TcpEvaluationSuite")
+  ;
+  return tid;
+}
+
+TrafficParameters::TrafficParameters (void)
+{
+}
+
+TrafficParameters::~TrafficParameters (void)
+{
+}
+
+void
+TrafficParameters::SetNumOfTmixFlows (uint32_t nTmixFlow)
+{
+  m_nTmixFlow = nTmixFlow;
+}
+
+uint32_t
+TrafficParameters::GetNumOfTmixFlows (void) const
+{
+  return m_nTmixFlow;
+}
+
+void
+TrafficParameters::SetNumOfTmixNodes (uint32_t nTmixNode)
+{
+	m_nTmixNode = nTmixNode;
+}
+
+uint32_t
+TrafficParameters::GetNumOfTmixNodes (void) const
+{
+  return m_nTmixNode;
+}
+
+void
+TrafficParameters::SetTmixCvName (std::string tmixCvName)
+{
+	m_tmixCvName = tmixCvName;
+}
+
+std::string
+TrafficParameters::GetTmixCvName (void) const
+{
+  return m_tmixCvName;
+}
+
+void
+TrafficParameters::SetTmixStdCvName (std::string tmixStdCvName)
+{
+	m_tmixStdCvName = tmixStdCvName;
+}
+
+std::string
+TrafficParameters::GetTmixStdCvName (void) const
+{
+  return m_tmixStdCvName;
+}
+
+void
+TrafficParameters::SetTmixAgentType (std::string tmixAgentType)
+{
+	m_tmixAgentType = tmixAgentType;
+}
+
+std::string
+TrafficParameters::GetTmixAgentType (void) const
+{
+  return m_tmixAgentType;
+}
+
+void
+TrafficParameters::SetTmixPacketSize (uint32_t tmixPacketSize)
+{
+	m_tmixPacketSize = tmixPacketSize;
+}
+
+uint32_t
+TrafficParameters::GetTmixPacketSize (void) const
+{
+  return m_tmixPacketSize;
+}
+
+void
+TrafficParameters::SetAqmTarget (uint32_t tmixPacketSize)
+{
+	m_aqmTarget = tmixPacketSize;
+}
+
+uint32_t
+TrafficParameters::GetAqmTarget (void) const
+{
+  return m_aqmTarget;
+}
+
+void
+TrafficParameters::SetAqmUsed (bool useAqm)
+{
+  m_useAqm = useAqm;
+}
+
+bool
+TrafficParameters::IsAqmUsed (void) const
+{
+  return m_useAqm;
+}
 
 }
 
--- src/tcp-eval/model/traffic-parameters.h
+++ src/tcp-eval/model/traffic-parameters.h
@@ -2,9 +2,95 @@
 #ifndef TRAFFIC_PARAMETERS_H
 #define TRAFFIC_PARAMETERS_H
 
+#include <stdint.h>
+#include "ns3/core-module.h"
+
 namespace ns3 {
 
-/* ... */
+/**
+ * \brief Initializes the traffic parameters.
+ *
+ * This class consists of setters and getters for traffic parameters
+ * The object of this class is passed to the methods where traffic
+ * parameters are used
+ */
+class TrafficParameters : public Object
+{
+public:
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+  static TypeId GetTypeId (void);
+
+  /**
+   * \brief Constructor
+   */
+  TrafficParameters (void);
+
+  /**
+   * \brief Destructor
+   */
+  ~TrafficParameters (void);
+
+  void SetNumOfTmixFlows (uint32_t nTmixFlow);
+
+  uint32_t GetNumOfTmixFlows (void) const;
+
+  void SetNumOfTmixNodes (uint32_t nTmixNode);
+
+  uint32_t GetNumOfTmixNodes (void) const;
+
+  void SetTmixCvName (std::string tmixCvName);
+
+  std::string GetTmixCvName (void) const;
+
+  void SetTmixStdCvName (std::string tmixStdCvName);
+
+  std::string GetTmixStdCvName (void) const;
+
+  void SetTmixAgentType (std::string tmixAgentType);
+
+  std::string GetTmixAgentType (void) const;
+
+  void SetTmixPacketSize (uint32_t tmixPacketSize);
+
+  uint32_t GetTmixPacketSize (void) const;
+
+  void SetAqmTarget (uint32_t tmixPacketSize);
+
+  uint32_t GetAqmTarget (void) const;
+
+  /**
+   * \brief Set whether AQM is to be used
+   *
+   * \param useAqm True if AQM is enabled
+   */
+  void SetAqmUsed (bool useAqm);
+
+  /**
+    * \brief Check whether AQM is to be used
+    *
+    * \return True if AQM is enabled
+    */
+  bool IsAqmUsed (void) const;
+
+
+private:
+  uint32_t     m_nTmixFlow;
+  uint32_t     m_nTmixNode;
+  std::string  m_tmixCvName;       //may be list  dkm
+  std::string  m_tmixStdCvName;    //may be list  dkm
+  std::string  m_tmixAgentType;
+  uint32_t     m_tmixPacketSize;
+  uint32_t     m_tmixDebugOutput;   //dkm required or not? and maybe list
+
+  std::string  m_testTcp;			//dkm required or not?
+  std::string  m_standardTcp;		//dkm required or not?
+
+  bool         m_useAqm;                 //!< Enable or disable AQM in routers
+  uint32_t     m_aqmTarget;			//dkm
+};
 
 }
 
